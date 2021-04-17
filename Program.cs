ad5124d405c69becd3637073b2c4b2e637432531
Jan Dobš <Jenda2003@gmail.com>
2021-04-17 16:11:16 +0200

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Person_walking_down_stairs
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(Person(i));
+            }
+            Console.ReadKey(true);
+        }
+        static string Person(int i)
+        {
+            string spacing = new string('\t', i);
+            string person = $@"
+{spacing}* O
+{spacing}*/|\
+{spacing}*/ \
+{spacing}******";
+            return person.TrimStart('\n');
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Person walking down stairs
+An exercise from [this tweet](https://www.answersdocs.com/ExpertAnswers/2-write-program-using-single-loop-person-walk-stairs-statements-needed-need-make-man-go-6-q40163935-)
+
+## Rules
+* Use only one `for` loop and no `if` statement
+* Every time when `person` walks down 1 stair, move him to the right a bit
+
+## How to run
+* Just download the program from [releases](https://github.com/jtheserg/Person-walking-down-stairs/releases) and run it
